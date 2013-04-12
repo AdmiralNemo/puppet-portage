b84a16fb100e5f3bc6cc9e52d603ea79e7a6fc37
Dustin C. Hatch <admiralnemo@gmail.com>
2013-04-12 11:40:08 -0500

    Allow make.conf variables to be redefined
    In cases where node definitions may be inheritied from others, both of
    which may define the same make.conf variable, it was not possible for
    both values to appear in the resulting file. This change differentiates
    the resource name from the variable name, allowing additional
    definitions for the same variable by changing the resource name and
    specifying the variable in the target parameter.
    
    In order for this to be effective, however, subsequent definitions must
    a) appear after the original definition, and b) contain a reference to
    the original definition in their content. This change permits a) by
    exposing the order parameter for the underlying concat::fragment
    resource. Users must take care to satisfy b).
    
    Example:
    
        host basehost {
          portage::makeconf { 'use':
            ensure  => present,
            content => 'mmx sse sse2',
          }
        }
    
        host hosta inherits basehost {
          portage::makeconf { 'myuse':
            ensure  => present,
            target  => use,
            content => '3dnow 3dnowext -X',
            order   => 20,
          }
        }

--- manifests/makeconf.pp
+++ manifests/makeconf.pp
@@ -4,7 +4,10 @@
 #
 # == Parameters
 #
-# [*name*]
+# [*target*]
+#
+# (**Namevar**: If omitted, this parameter's value defaults to the resource's
+# title.)
 #
 # The name of the make.conf variable
 #
@@ -17,6 +20,10 @@
 #
 # The ensure state of the makeconf section.
 #
+# [*order*]
+#
+# The order in which the variable should appear in make.conf
+#
 # == Example
 #
 #     portage::makeconf { 'use':
@@ -26,14 +33,22 @@
 
 define portage::makeconf(
   $ensure = present,
+  $target = '',
   $content = '',
+  $order = 10,
 ) {
   include concat::setup
   include portage
+
+  $var_name = $target ? {
+    ''      => $name,
+    default => $target,
+  }
 
   concat::fragment { $name:
     ensure  => $ensure,
     content => template('portage/makeconf.conf.erb'),
     target  => $portage::make_conf,
+    order   => $order,
   }
 }
